b82e3181cea54313de9acdf36ded39be5f6d5b92
Polina <polinakvyat9@gmail.com>
2026-09-21 18:28:52 +0300

    Commit

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,7 +1,3 @@
-import Link from 'next/link';
-import css from './SidebarNotes.module.css';
-import { usePathname } from 'next/navigation';
-
 const tags = ['Todo', 'Work', 'Personal', 'Meeting', 'Shopping'];
 
 
@@ -15,17 +11,35 @@ export default function Sidebar({ tags = ['Work', 'Todo', 'Personal'] }: Sidebar
   // Список усіх тегів із додаванням системного тегу 'all'
   const allTags = ['all', ...tags];
 
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import css from './SidebarNotes.module.css';
+
+export default function SidebarDefault() {
+  const pathname = usePathname();
+
   return (
     <aside className={css.sidebar}>
-      <h3 className={css.title}>Tags</h3>
+      <h3 className={css.title}>Categories</h3>
       <nav className={css.nav}>
         <ul className={css.tagList}>
-          {allTags.map((tag) => {
+          <li className={css.tagItem}>
+            <Link
+              href="/notes/filter/all"
+              className={`${css.tagLink} ${
+                pathname === '/notes/filter/all' || pathname === '/notes/filter'
+                  ? css.active
+                  : ''
+              }`}
+            >
+              All notes
+            </Link>
+          </li>
+          {tags.map((tag) => {
             const href = `/notes/filter/${encodeURIComponent(tag)}`;
-            
-            const isActive =
-              pathname === href ||
-              (tag === 'all' && (pathname === '/notes/filter' || pathname === '/notes/filter/all'));
+            const isActive = pathname === href;
 
             return (
               <li key={tag} className={css.tagItem}>
@@ -33,7 +47,7 @@ export default function Sidebar({ tags = ['Work', 'Todo', 'Personal'] }: Sidebar
                   href={href}
                   className={`${css.tagLink} ${isActive ? css.active : ''}`}
                 >
-                  {tag === 'all' ? 'All notes' : `#${tag}`}
+                  #{tag}
                 </Link>
               </li>
             );
